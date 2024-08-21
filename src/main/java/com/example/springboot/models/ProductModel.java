caff515d351fc87670dc2a591a364efc7e0c7f19
GSacata <gsacata@gmail.com>
2024-08-21 20:41:53 -0300

    done modeling product

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,53 @@
+package com.example.springboot.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TB_PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L; // Controle de versão da classe/modelo
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO) // IDs serão gerados automaticamente
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+    
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+    
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public BigDecimal getValue() {
+        return value;
+    }
+    
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    
+}
