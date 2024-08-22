c43835ad5bf6d5a5f554d873f67a5bf9fe3a4c43
GSacata <gsacata@gmail.com>
2024-08-22 20:05:51 -0300

    Creating controller file

---
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -0,0 +1,5 @@
+package com.example.springboot.controllers;
+
+public class ProductController {
+    
+}
--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -1,5 +1,12 @@
 package com.example.springboot.repositories;
 
-public interface ProductRepository {
+import com.example.springboot.models.ProductModel;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+    
 }
