36a3af11f8f8d69e9cc008b2d9876833f13af89e
GSacata <gsacata@gmail.com>
2024-08-22 21:19:44 -0300

    post method working

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,9 +1,18 @@
 package com.example.springboot.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot.dtos.ProductRecordDTO;
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -11,6 +20,11 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository; // ponto de injeção para termos acesso aos métodos JPA
 
-    
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDTO, productModel); // Converte de DTO para model
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
     
 }
