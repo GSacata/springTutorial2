36c82adbc42fb92d106faaf68b05378c4eb0047d
GSacata <gsacata@gmail.com>
2024-10-01 15:32:22 -0300

    ajustes antes de login

--- src/main/java/com/example/springboot/infra/security/SecurityConfigs.java
+++ src/main/java/com/example/springboot/infra/security/SecurityConfigs.java
@@ -2,6 +2,7 @@ package com.example.springboot.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfigs {
         return httpSecurity
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                )
             .build();
     }
 }
