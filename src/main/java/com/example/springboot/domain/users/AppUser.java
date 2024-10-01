3c53f6e59c68a07c561bb580368bdc2c0c0fc7a3
GSacata <gsacata@gmail.com>
2024-10-01 15:26:02 -0300

    implementando configs de segurança

--- src/main/java/com/example/springboot/domain/users/AppUser.java
+++ src/main/java/com/example/springboot/domain/users/AppUser.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 // import lombok.AllArgsConstructor;
 // import lombok.EqualsAndHashCode;
@@ -24,7 +27,10 @@ import jakarta.persistence.Table;
 // @EqualsAndHashCode(of="id")
 public class AppUser implements UserDetails{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    
     private String login;
     private String password;
     private AppUserRoles role; // Será feito enum
